a6c8661d75176d19b418c5478cd6c797a2a33479
Aditya <adityabodewar835@gmail.com>
2025-12-28 12:34:25 +0530

    login api created

--- Backend/application/__init__.py
+++ Backend/application/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from application.db import conn
+from application.cloudinary import cloud_Connectione
 
 app=Flask(__name__)
 CORS(app)
 conn()
+cloud_Connectione()
 from application import Routes
-
+from application import UserRoutes
 

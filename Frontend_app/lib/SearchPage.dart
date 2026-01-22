ca78c559b907dd5fa35060fed04e26845a35eeee
priyanshu bhandare <pbhandare239@gmail.com>
2026-01-22 23:00:47 +0530

    update

--- Frontend_app/lib/SearchPage.dart
+++ Frontend_app/lib/SearchPage.dart
@@ -16,7 +16,6 @@ class _SearchPageState extends State<SearchPage> {
     "Cotton Fabric",
     "Silk Dupatta"
   ];
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);

18f773eab1e21afdcd19991e1aea436977e6da4d
Aditya <adityabodewar835@gmail.com>
2026-01-29 22:51:07 +0530

    Login Register Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,31 @@ import NavBar from './Components/NavBar'
 import LandingPage from './Pages/LandingPage'
 import HomePage from './Pages/HomePage'
 import BottomNavigation from './Components/BottomNavigation'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import LoginPage from './Pages/LoginPage'
+import ProfilePage from './Pages/ProfilePage'
+import Register from './Components/Register'
+
 
 const App = () => {
   return (
-  <div className='bg-white text-black'>
-  <HomePage/>
-   </div>
+ <>
+ 
+ <Router>
+
+
+  <Routes>
+
+    <Route path='/' element={<HomePage/>}/>
+    <Route path='/Login' element={<LoginPage/>}/>
+    <Route path='/Profile' element={<ProfilePage/>}/>
+    <Route path='/Register' element={<Register/>}/>
+  </Routes>
+ </Router>
+ 
+ 
+ 
+ </>
   )
 }
 
---
+++ Frontend/src/Pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../Components/login'
+
+const LoginPage = () => {
+  return (
+    <>
+    
+    <Login/>
+    
+    </>
+  )
+}
+
+export default LoginPage

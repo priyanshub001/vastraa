3bff638822aa62ab011667cce70c9447e9a59c89
priyanshu bhandare <pbhandare239@gmail.com>
2026-02-01 20:56:11 +0530

    update

--- Frontend_app/lib/loginPage.dart
+++ Frontend_app/lib/loginPage.dart
@@ -100,7 +100,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
 
               const SizedBox(height: 20),
-
               Center(
                 child: TextButton(
                   onPressed: () {
